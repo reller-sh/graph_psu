5fdbe72cef3af7182f1086e90a9ea33dcdadc392
Vlad.sh <shkerdin.v@yandex.ru>
2021-03-23 10:54:16 +0300

    adв pic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import { Layer, Shape, Stage } from "react-konva";
 import { useForm } from "react-hook-form";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-const p = [[10, 10, false], [10, 50, false], [100, 20, true], [150, 50, true]]
+const p = [
+    [159, 156, false, 1],
+    [159, 100, true, 1],
+    [180, 100, true, 1],
+    [180, 145, true, 1],
+    [220, 125, false, 1],
+    [130, 170, true, 1],
+    [130, 270, true, 1],
+    [310, 270, true, 1],
+    [310, 170, true, 1],
+    [130, 170, true, 1],
+    [220, 125, true, 1],
+    [310, 170, true, 1],
+    [235, 210, false, 1],
+    [205, 210, true, 1],
+    [205, 240, true, 1],
+    [235, 240, true, 1],
+    [235, 210, true, 1],
+
+    // pattern:
+    [0, 0, false, 1],
+]
 
 function App() {
     const {register, handleSubmit, watch} = useForm({
@@ -15,20 +36,19 @@ function App() {
 
     const {rotate, glass, size_x} = watch()
 
-    const [points, setPoints] = useState([[10, 10, false, 1], [10, 50, false, 1], [100, 20, true, 1], [150, 50, true, 1]]);
+    const [points, setPoints] = useState([...p]);
 
-    useEffect(() => {
-        setPoints([...p.map(([x, y, isDraw, thirdParty]) => {
+    const rotation = () => {
+        setPoints(s => [...s.map(([x, y, isDraw, thirdParty]) => {
             const g = Math.PI * (rotate) / 180
             console.log(points, g);
             const nx = (x * Math.cos(g) + y * Math.sin(g))
             const ny = x * -Math.sin(g) + y * Math.cos(g)
             return [nx, ny, isDraw, thirdParty]
         })])
-        // eslint-disable-next-line
-    }, [rotate])
+    }
 
-    useEffect(() => {
+    const glassing = () => {
         setPoints(s => [...s.map(([x, y, isDraw, thirdParty]) => {
             if (glass) {
                 const g = Math.PI
@@ -38,28 +58,31 @@ function App() {
             } else
                 return [x, y, isDraw, thirdParty]
         })])
-    }, [glass])
+    }
 
-    useEffect(() => {
-        setPoints([...p.map(([x, y, isDraw, thirdParty]) => {
-                const nx = x * size_x
-                return [nx, y, isDraw, thirdParty]
+    const resizer = () => {
+        setPoints(s => [...s.map(([x, y, isDraw, thirdParty]) => {
+            const nx = x * size_x
+            return [nx, y, isDraw, thirdParty]
         })])
-    }, [size_x])
+    }
 
     return (
         <div className="App">
             <div onSubmit={handleSubmit(() => {
             })}>
                 Увеличение по x: {'  '}
-                <input ref={register()} name='size_x' step={0.02} min={0.01} type="number"/>
+                <input ref={register()} name='size_x' onChange={() => resizer()} step={0.02} min={0.01} type="number"/>
                 <br/>
                 Поворот: {'  '}
-                <input ref={register()} name='rotate' type="number"/>
+                <input ref={register()} name='rotate' type="number" onChange={() => rotation()}/>
                 Отражение: {'  '}
-                <input ref={register()} name='glass' type='checkbox'/>
+                <input ref={register()} name='glass' type='checkbox' onChange={() => glassing()}/>
+                <br />
+                Вернуть к истокам: {'  '}
+                <input ref={register()} name='glass' type='checkbox' onChange={() => setPoints([...p])}/>
             </div>
-            <Stage height={'2000'} width={'2000'}>
+            <Stage height={'900'} width={'1920'}>
                 <Layer>
                     <Shape
                         sceneFunc={(context, shape) => {
